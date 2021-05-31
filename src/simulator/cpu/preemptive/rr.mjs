b19269f349af1bbf0723876ab219eddb3e49640c
tdherring <tomherring1999@gmail.com>
2021-05-31 22:40:35 +0100

    Simplified and cleaned up RR code

--- src/simulator/cpu/preemptive/rr.mjs
+++ src/simulator/cpu/preemptive/rr.mjs
@@ -19,35 +19,26 @@ class RR extends PreemptiveScheduler {
         let name = p.getName();
         let burstTime = p.getBurstTime();
 
+        // Only run this process if it has execution time remaining.
         if (burstTime > 0) {
           if (verbose) console.log("[" + timeDelta + "] Spawned Process", name);
-          // Applying a full quantum would take this processes burstTime below 0, so only execute for the burstTime.
-          if (burstTime <= this.timeQuantum) {
+
+          if (burstTime <= this.timeQuantum || burstTime - this.timeQuantum <= 0) {
+            // Applying a full quantum would take this processes burstTime below 0, so only execute for the burstTime -OR-
+            // Process will run to completion after this quantum.
             this.schedule.push({ processName: name, timeDelta: timeDelta, arrivalTime: 0, burstTime: burstTime });
             timeDelta += burstTime;
-            // burstTime - burstTime = 0
             p.setBurstTime(0);
-            if (verbose) console.log("[" + timeDelta + "] Process", name, "finished executing!");
           } else {
-            // Process will run to completion after this quantum.
-            if (burstTime - this.timeQuantum <= 0) {
-              this.schedule.push({ processName: name, timeDelta: timeDelta, arrivalTime: 0, burstTime: burstTime });
-              timeDelta += burstTime;
-              // burstTime - burstTime = 0
-              p.setBurstTime(0);
-            }
             // Process still has time remaining after this quantum.
-            else {
-              this.schedule.push({ processName: name, timeDelta: timeDelta, arrivalTime: 0, burstTime: this.timeQuantum });
-              timeDelta += this.timeQuantum;
-              p.setBurstTime(burstTime - this.timeQuantum);
-            }
-
-            if (verbose) console.log("[" + timeDelta + "] Process", name, "finished executing!");
+            this.schedule.push({ processName: name, timeDelta: timeDelta, arrivalTime: 0, burstTime: this.timeQuantum });
+            timeDelta += this.timeQuantum;
+            p.setBurstTime(burstTime - this.timeQuantum);
           }
-        }
 
-        if (burstTime > 0) endDispatch = false;
+          if (verbose) console.log("[" + timeDelta + "] Process", name, "finished executing!");
+          endDispatch = false;
+        }
       }
 
       // If all processes have executed to completion (all burstTimes are 0)
@@ -58,14 +49,15 @@ class RR extends PreemptiveScheduler {
 
 // Syntax for use on frontend.
 
-let test_rr = new RR(4);
+let test_rr = new RR(5);
 
 // ! Arrival time for RR is always 0 (parameter 2 of createProcess)
-// TODO: Implement priority for RR
+// TODO: Implement arrival times for RR
 
-test_rr.createProcess("p1", 0, 24);
+test_rr.createProcess("p1", 0, 21);
 test_rr.createProcess("p2", 0, 3);
-test_rr.createProcess("p3", 0, 3);
+test_rr.createProcess("p3", 0, 6);
+test_rr.createProcess("p4", 0, 2);
 
 test_rr.dispatchProcesses(true);
 test_rr.outputGraphicalRepresentation();
