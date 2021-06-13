8d2ed8e393986e857c2d819abbdb14be1432d8bc
tdherring <tomherring1999@gmail.com>
2021-06-13 21:09:18 +0100

    Rewrote RR to support arrival times

--- src/simulator/cpu/preemptive/rr.mjs
+++ src/simulator/cpu/preemptive/rr.mjs
@@ -8,39 +8,78 @@ class RR extends PreemptiveScheduler {
 
   dispatchProcesses(verbose = false) {
     if (verbose) console.log("\nOSSAT-RR\n-----------------------------------------");
+    this.jobQueue = this.sortProcessesByArrivalTime(this.jobQueue);
     let timeDelta = 0;
-    let endDispatch = false;
+    let i = 0;
+    let readyQueue = [];
 
-    // Execute until all processes have been dispatched and run for their full burst time.
-    while (!endDispatch) {
-      let numIters = this.jobQueue.length;
-      endDispatch = true;
+    let availableProcesses = this.getAvailableProcesses(this.jobQueue, timeDelta);
 
-      for (let i = 0; i < numIters; i++) {
-        let p = this.jobQueue[i];
-        let name = p.getName();
-        let burstTime = p.getBurstTime();
+    if (availableProcesses.length > 0) {
+      // If processes are available from time delta 0.
+      // Initialise the ready queue to hold all processes which are available at this time delta (0).
+      readyQueue = this.getAvailableProcesses(this.jobQueue, timeDelta);
+    } else {
+      // Otherwise, we need to idle at the first iteration, so set the first item in the ready queue to the process which arrives quickest.
+      readyQueue.push(this.jobQueue[0]);
+    }
 
-        // Only run this process if it has execution time remaining.
-        if (burstTime > 0) {
-          if (verbose) console.log("[" + timeDelta + "] Spawned Process", name);
+    // Keep scheduling until all processes have no burst time left.
+    while (this.jobQueue.filter((x) => x.getBurstTime() != 0).length > 0) {
+      let p = readyQueue[i];
+      let name = p.getName();
+      let arrivalTime = p.getArrivalTime();
+      let burstTime = p.getBurstTime();
 
-          if (burstTime <= this.timeQuantum || burstTime - this.timeQuantum <= 0) {
-            // Applying a full quantum would take this processes burst time below 0, so only execute for the burst time -OR-
-            // Process will run to completion after this quantum, so only execute for the burst time.
-            this.schedule.push({ processName: name, timeDelta: timeDelta, arrivalTime: 0, burstTime: burstTime });
-            timeDelta += burstTime;
-            p.setBurstTime(0);
-          } else {
-            // Process still has time remaining after this quantum.
-            this.schedule.push({ processName: name, timeDelta: timeDelta, arrivalTime: 0, burstTime: this.timeQuantum });
-            timeDelta += this.timeQuantum;
-            p.setBurstTime(burstTime - this.timeQuantum);
-          }
+      // Check whether the CPU needs to idle for the next process.
+      if (arrivalTime > timeDelta) {
+        if (verbose) console.log("[" + timeDelta + "] CPU Idle...");
+        this.schedule.push({ processName: "IDLE", timeDelta: timeDelta, arrivalTime: null, burstTime: arrivalTime - timeDelta });
+        // Adjust time delta with respect to idle length.
+        timeDelta += arrivalTime - timeDelta;
+      }
 
-          if (verbose) console.log("[" + timeDelta + "] Process", name, "finished executing!");
-          endDispatch = false;
+      // Track how much to adjust the time delta.
+      let deltaIncrement = 0;
+
+      // If the process has time left to execute.
+      if (burstTime > 0) {
+        if (verbose) console.log("[" + timeDelta + "] Spawned Process", name);
+
+        if (burstTime <= this.timeQuantum) {
+          // The process will run to completion quicker than a full quantum.
+          deltaIncrement = burstTime;
+          p.setBurstTime(0);
+        } else {
+          // A full quantum won't run the process to completion.
+          deltaIncrement = this.timeQuantum;
+          p.setBurstTime(burstTime - this.timeQuantum);
         }
+
+        // Increment the queue head pointer.
+        i++;
+
+        this.schedule.push({ processName: name, timeDelta: timeDelta, arrivalTime: arrivalTime, burstTime: deltaIncrement });
+        timeDelta += deltaIncrement;
+        if (verbose) console.log("[" + timeDelta + "] Process", name, "finished executing!");
+      }
+
+      // Find all processes which are available at this timestep (diff the arrays).
+      let newlyAvailable = this.getAvailableProcesses(this.jobQueue, timeDelta).filter((x) => this.getAvailableProcesses(this.jobQueue, timeDelta - deltaIncrement).indexOf(x) === -1);
+
+      // If after this quantum there are new processes available, add the the front of the ready queue.
+      if (newlyAvailable.length > 0) {
+        readyQueue = readyQueue.concat(newlyAvailable);
+      }
+
+      // If the process still has execution time remaining after this quantum, add it to the end of the ready queue.
+      if (p.getBurstTime() > 0) {
+        readyQueue.push(p);
+      }
+
+      // Finally, if the readyQueue is "empty", add the process with the nearest arrival time which has execution time remaining.
+      if (readyQueue.length - 1 < i) {
+        readyQueue.push(this.sortProcessesByArrivalTime(this.jobQueue.filter((x) => x.burstTime > 0))[0]);
       }
     }
   }
@@ -50,13 +89,10 @@ class RR extends PreemptiveScheduler {
 
 let test_rr = new RR(5);
 
-// ! Arrival time for RR is always 0 (parameter 2 of createProcess)
-// TODO: Implement arrival times for RR
-
-test_rr.createProcess("p1", 0, 21);
-test_rr.createProcess("p2", 0, 3);
-test_rr.createProcess("p3", 0, 6);
-test_rr.createProcess("p4", 0, 2);
+test_rr.createProcess("p1", 4, 4);
+test_rr.createProcess("p2", 0, 10);
+test_rr.createProcess("p3", 17, 17);
+test_rr.createProcess("p4", 2, 18);
 
 test_rr.dispatchProcesses(true);
 test_rr.outputGraphicalRepresentation();
